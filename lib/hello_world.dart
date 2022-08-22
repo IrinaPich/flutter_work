9a71894a880f6efe180212c32431fe9ed83227d9
Irina Pichugina <pichuginaiv@gmail.com>
2022-08-22 14:56:01 +0200

    Task: print Hello World

---
+++ lib/hello_world.dart
@@ -0,0 +1,10 @@
+// Define a function.
+void printText(String printedText) {
+  print('The printed text is $printedText'); // Print to console.
+}
+
+// This is where the app starts executing.
+void main() {
+  var text = "Hello, World!"; // Declare and initialize a variable.
+  printText(text); // Call a function.
+}
