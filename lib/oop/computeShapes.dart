96b88efbc24f487f46aa57a58ea35e7cf7b7de24
Irina Pichugina <pichuginaiv@gmail.com>
2022-09-10 16:13:11 +0200

    OOP - cases input

---
+++ lib/oop/computeShapes.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+import 'package:flutter_work/oop/shapeHierarchy.dart';
+
+void readShapeList() {
+  String? input;
+  while (input != '') {
+    print('Please enter the Figure Name or click Enter if you finished your list');
+    input = stdin.readLineSync();
+    //List<String> shapesList = ['qq'];
+
+    switch (input) {
+      case '':
+        // print list of shapes and calculation
+        //List content = calculateFigures(parameter);
+        break;
+
+      case 'Equilateral Triangle':
+        print('Please enter parameters .... ');
+        // add shape name and input (parameters) to the list
+        break;
+
+      case 'Scalene triangle':
+        print('Please enter parameters .... ');
+        // add shape name and input (parameters) to the list
+        break;
+
+      case 'Isosceles triangle':
+        print('Please enter parameters.... ');
+        // add shape name and input (parameters) to the list
+        break;
+
+      case 'Rectangle':
+        print('Please enter parameters .... ');
+        // add shape name and input (parameters) to the list
+        break;
+
+      case 'Square':
+        print('Please enter parameters .... ');
+        // add shape name and input (parameters) to the list
+        break;
+
+      case 'Ellipsoid':
+        print('Please enter parameters .... ');
+        // add shape name and input (parameters) to the list
+        break;
+
+      case 'Circle':
+        print('Please enter parameters .... ');
+        // add shape name and input (parameters) to the list
+        break;
+
+      default:
+        print("Figure name \"$input\" not recognized");
+        break;
+    }
+  }
+}
+
+void main() {
+  readShapeList();
+}
