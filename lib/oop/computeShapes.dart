fc76b24dabacb300d775aef18f54a2e2817ee7a6
Irina Pichugina <pichuginaiv@gmail.com>
2022-10-10 20:33:56 +0200

    UI for Isosceles Triangle

--- lib/oop/computeShapes.dart
+++ lib/oop/computeShapes.dart
@@ -1,20 +1,21 @@
 import 'dart:io';
 import 'package:flutter_work/oop/shapeHierarchy.dart';
 
-/*void readShapeList() {
+List<Figure> readShapeList() {
   String? input;
+  List<Figure> figures = [];
   while (input != '') {
-    print('Please enter the Figure Name or click Enter if you finished your list');
+    print(
+        'Please enter the Figure Name or click Enter if you finished your list');
     input = stdin.readLineSync();
+
     //List<String> shapesList = ['qq'];
 
     switch (input) {
       case '':
-        // print list of shapes and calculation
-        //List content = calculateFigures(parameter);
         break;
 
-      case 'Equilateral Triangle':
+/*      case 'Equilateral Triangle':
         String figureName = 'Equilateral triangle';
         EquilateralTriangle p = EquilateralTriangle(name);
         break;
@@ -22,14 +23,46 @@ import 'package:flutter_work/oop/shapeHierarchy.dart';
       case 'Scalene Triangle':
         print('Please enter parameters .... ');
         // add shape name and input (parameters) to the list
-        break;
+        break;*/
 
       case 'Isosceles Triangle':
-        String figureName = 'Isosceles triangle';
-        IsoscelesTriangle p = IsoscelesTriangle(figureName);
+        String name = 'Isosceles Triangle';
+        String? parameters;
+
+        print('Please enter Height and Base of $name separated by a space');
+        parameters = stdin.readLineSync();
+
+        while (parameters == null || parameters.isEmpty) {
+          print(
+              'Please enter correct parameters Height and Base of $name separated by a space');
+          parameters = stdin.readLineSync();
+          if (parameters == null || parameters.isEmpty) {
+            print('Parameters Height and Base are incorrect ');
+            continue;
+          }
+        }
+        var splitParameters = parameters.split(' ');
+        if (splitParameters.length < 2) {
+          print('Parameters Height and Base are incorrect ');
+          continue;
+        }
+        var heightString = splitParameters[0];
+        var baseString = splitParameters[1];
+
+        double? height = double.tryParse(heightString);
+        double? base = double.tryParse(baseString);
+
+        if (height == null || base == null) {
+          print('Parameters Height and Base are incorrect ');
+          continue;
+        }
+
+        var t = IsoscelesTriangle(name, height, base);
+        figures.add(t);
+
         break;
 
-      case 'Rectangle':
+/*      case 'Rectangle':
         print('Please enter parameters .... ');
         // add shape name and input (parameters) to the list
         break;
@@ -47,15 +80,17 @@ import 'package:flutter_work/oop/shapeHierarchy.dart';
       case 'Circle':
         print('Please enter parameters .... ');
         // add shape name and input (parameters) to the list
-        break;
+        break;*/
 
       default:
         print("Figure name \"$input\" not recognized");
         break;
     }
   }
+  return figures;
 }
 
 void main() {
-  readShapeList();
-}*/
+  var result = readShapeList();
+  print(result);
+}
